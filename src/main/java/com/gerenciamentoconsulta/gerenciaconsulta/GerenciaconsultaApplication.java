109a1a8de96eba5929794dfecbb93264bb023616
Felipe Rodrigues <felipebatista.dev00@gmail.com>
2025-05-23 12:06:08 -0300

    correção de bugs

--- src/main/java/com/gerenciamentoconsulta/gerenciaconsulta/GerenciaconsultaApplication.java
+++ src/main/java/com/gerenciamentoconsulta/gerenciaconsulta/GerenciaconsultaApplication.java
@@ -1,4 +1,4 @@
-package com.gerenciamentoconsulta.gerenciaconsulta.controller;
+package com.gerenciamentoconsulta.gerenciaconsulta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/gerenciamentoconsulta/gerenciaconsulta/repository/ConsultaRepository.java
+++ src/main/java/com/gerenciamentoconsulta/gerenciaconsulta/repository/ConsultaRepository.java
@@ -4,6 +4,8 @@ package com.gerenciamentoconsulta.gerenciaconsulta.repository;
 
 import com.gerenciamentoconsulta.gerenciaconsulta.model.Consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
 }
